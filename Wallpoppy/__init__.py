43ffc5fc8fb2d5b515b2b9d75e6871eb8c5d582f
Hattshire <hattshire@users.noreply.github.com>
2018-12-24 20:36:37 -0300

    Initial conversion from script

---
+++ Wallpoppy/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+## Public domain
+# See https://unlicense.org and UNLICENSE.txt
+__license__ = "Unlicense"
+__docformat__ = "reStructuredText"
+
+
+def run():
+	"""Run this module.
+
+	:returns: Nothing
+	:rtype: none
+	.. todo::Implement this.
+	"""
+	return
---
+++ Wallpoppy/__main__.py
@@ -0,0 +1,7 @@
+## Public domain
+# See https://unlicense.org and UNLICENSE.txt
+
+
+# Assume this file was invoked as a whole module
+# So, __init__:run() is available
+run()
